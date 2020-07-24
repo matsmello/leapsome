d53fb9e49065c59078bcdde9e0aeb37619cd4602
Matheus Mello <matheusmello_22@hotmail.com>
2020-07-23 21:32:07 -0300

    Create basic html structure on main page

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,7 +1,29 @@
 import React from "react";
 
-const Main: React.FC = () => {
-  return <h1>Main</h1>;
-};
+const Main: React.FC = () => (
+  <Container>
+    <SideMenu>
+      <header>
+        <img />
+        <h1>Lucy Andrews</h1>
+        <strong>Head of Sales</strong>
+      </header>
+      <Button></Button>
+
+      <h3>Direct colleagues</h3>
+      <SimpleListItem />
+      <SimpleListItem />
+    </SideMenu>
+    <Card>
+      <header>
+        <h1>Good evening, Lucy! 👋</h1>
+      </header>
+      <ListItem />
+      <footer>
+        <strong>MORE</strong>
+      </footer>
+    </Card>
+  </Container>
+);
 
 export default Main;
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,5 +1,29 @@
 import { createGlobalStyle } from "styled-components";
 
 export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    outline: 0;
+  }
 
+  body {
+    background: rgb(245,245,245);
+    color: black;
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, button {
+    font-family: 'Lato', sans-serif;
+    font-size: 16px;
+  }
+
+  h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 700;
+  }
+
+  button {
+    cursor: pointer;
+  }
 `;
